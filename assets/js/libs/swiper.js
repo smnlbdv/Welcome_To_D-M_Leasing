e9f40caadbbe963f80db19a0af4267a1fd28f28b
Semen Lebedew <mamontedinorog@yandex.by>
2023-03-18 00:15:57 +0300

    pages_work

---
+++ assets/js/libs/swiper.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper('.swiper', {
+
+    pagination: {
+      el: '.swiper-pagination',
+    },  
+    effect:"fade",
+    autoplay : {
+        delay:3000,
+        disableInteraction: false,
+    }
+  });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
-let swiper = new Swiper('.swiper', {
 
-    pagination: {
-      el: '.swiper-pagination',
-    },  
-    effect:"fade",
-    autoplay : {
-        delay:3000,
-        disableInteraction: false,
-    }
+const sectionFilters = document.querySelectorAll('.filters-item')
+const carBlock = document.querySelectorAll('.car__block')
+
+  sectionFilters.forEach(item => {
+    item.addEventListener('click', open)
   });
+
+
+  function open (evt) {
+    const button = this.dataset.button
+    sectionFilters.forEach(item => {
+      item.classList.remove('filters-item--active')
+    });
+    this.classList.add('filters-item--active')
+    carBlock.forEach(item => {
+      item.classList.remove('car__block-active')
+    })
+    document.querySelector(`#${button}`).classList.add('car__block-active')
+  }
